2b1163f4672b160395f7103a79dcbe2f8efb6a9e
lerichter <lerichter.c@gmail.com>
2024-07-09 12:29:00 -0300

    fix:about

--- portfar/components/about/index.js
+++ portfar/components/about/index.js
@@ -1,34 +1,33 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
-import banner from '../../public/Images/banner.png'
+import banner from '../../public/Images/banner.png';
 
 const About = (props) => {
-
     return(
         <section id="about" className="tp-about-section">
             <div className="container">
-                <div className="rrow d-flex align-items-stretch">
+                <div className="row d-flex align-items-stretch">
                     <div className="col-lg-5 col-md-12 col-12 d-flex">
                         <div className="tp-about-wrap flex-fill">
-                        <div className="tp-about-img">
-                            <Image src={banner} alt=""/>
-                        </div>
+                            <div className="tp-about-img">
+                                <Image src={banner} alt="Banner Image" layout="responsive"/>
+                            </div>
                         </div>
                     </div>
                     <div className="col-lg-7 col-md-12 col-12 d-flex">
                         <div className="tp-about-text flex-fill">
-                        <div className="tp-about-icon">
-                            <i className="fi flaticon-web-design"></i>
-                        </div>
-                        <div className="tp-about-icon-content">
-                            <h2>Inovação e Tecnologia ao Seu Alcance</h2>
-                            <p>Nossa empresa é formada por especialistas em desenvolvimento web e apaixonados por tecnologia. <br></br>
-                            Com anos de experiência na criação de soluções digitais inovadoras, ajudamos empresas a transformar suas ideias em realidade através de sites modernos, funcionais e visualmente atraentes. <br></br>
-                            Com uma abordagem centrada no cliente, garantimos que cada projeto seja desenvolvido com excelência e atenção aos detalhes.<br></br><br></br>
-                            Se você busca uma presença digital forte e impactante, estamos aqui para ajudar! <br></br>
-                            Vamos transformar o seu projeto em um sucesso. <br></br><br></br>
-                            Entre em contato e descubra como posso contribuir para o crescimento do seu negócio.</p>
-                        </div>
+                            <div className="tp-about-icon">
+                                <i className="fi flaticon-web-design"></i>
+                            </div>
+                            <div className="tp-about-icon-content">
+                                <h2>Inovação e Tecnologia ao Seu Alcance</h2>
+                                <p>Nossa empresa é formada por especialistas em desenvolvimento web e apaixonados por tecnologia. <br></br>
+                                Com anos de experiência na criação de soluções digitais inovadoras, ajudamos empresas a transformar suas ideias em realidade através de sites modernos, funcionais e visualmente atraentes. <br></br>
+                                Com uma abordagem centrada no cliente, garantimos que cada projeto seja desenvolvido com excelência e atenção aos detalhes.<br></br><br></br>
+                                Se você busca uma presença digital forte e impactante, estamos aqui para ajudar! <br></br>
+                                Vamos transformar o seu projeto em um sucesso. <br></br><br></br>
+                                Entre em contato e descubra como posso contribuir para o crescimento do seu negócio.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
